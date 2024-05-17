c4f31de94b84e6b223e696fac32a73e202cf707a
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-05-17 21:41:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# Cursor.js
+
+A fancy cursor for your website
+
+![Cursor.js Demo](doc/demo.gif)
+
+## Quick Start
+
+Its really simple to use Cursor.js in your website. Just include the script and css file in your HTML and you are good to go.
+
+Add this link to your head tag in your HTML file.
+```html
+<link rel="stylesheet" href="https://raw.githubusercontent.com/0x4248/cursor.js/main/css/cursor.css">
+```
+
+Add this script tag to your body tag in your HTML file.
+```html
+<script src="https://raw.githubusercontent.com/0x4248/cursor.js/main/js/cursor.js"></script>
+```
+
+Then at the top of your body tag add the following code.
+```html
+<div class="cursor"></div>  
+<div class="cursor-inner"></div>
+```
+
+Your page should look like this.
+
+```html
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <!-- Everting else in head -->
+        <link rel="stylesheet" href="https://raw.githubusercontent.com/0x4248/cursor.js/main/css/cursor.css">
+    </head>
+    <body>
+        <div class="cursor"></div>  
+        <div class="cursor-inner"></div>    
+        <!-- Site HTML -->
+        <script src="https://raw.githubusercontent.com/0x4248/cursor.js/main/js/cursor.js" async defer></script>
+    </body>
+</html>
+```
+
+And you are done. Now you have a fancy cursor on your website.
+
+## Customization
+
+You can use the dark theme by adding the following below the css link in the head tag.
+```html
+<link rel="stylesheet" href="https://raw.githubusercontent.com/0x4248/cursor.js/main/css/dark-mode.css">
+```
+
+You can also change the color of the cursor by adding the following code below the css link in the head tag.
+```html
+.cursor {
+    background-color: rgba(226, 0, 0, 0.239);
+}
+
+.cursor-inner {
+    background-color: rgba(255, 152, 152, 0.447);
+}
+```
+
+Now our cursor will have a red color.
+
+![Cursor.js Red](doc/red.png)
+
+## Tooltips
+
+You can also add tooltips to your website. Just add the following code to your HTML file.
+
+![tooltip](doc/tooltip.png)
+
+Just simply call the function `showTooltip()` with the text you want to display as the argument.
+
+```html
+<button onmouseover="showTooltip('Click me')" onmouseout="hideTooltip()">Hover me</button>
+```
+
+## Licence
+
+This is licenced under the GNU General Public License v3.0. Please read the [LICENCE](LICENCE) file for more information.
---
+++ js/cursor.js
@@ -0,0 +1,113 @@
+/* Cursor.js 
+ * A fancy cursor for your website
+ * GitHub: https:///www.github.com/cursor.js
+ * Licence: GNU General Public License v3.0
+ * By: 0x4248
+ */
+
+let innerCursorPos = {
+    x: 0,
+    y: 0
+};
+
+let targetPos = {
+    x: 0,
+    y: 0
+};
+
+let tooltipEnabled = false;
+
+let scrollTimer = null;
+
+document.addEventListener('mousemove', (e) => {
+    const cursor = document.querySelector('.cursor');
+    if (!tooltipEnabled) {
+        cursor.style.left = `${e.clientX}px`;
+        cursor.style.top = `${e.clientY}px`;
+    } else {
+        cursor.style.left = `${e.clientX + 80}px`;
+        cursor.style.top = `${e.clientY + 40}px`;
+    }
+    targetPos.x = e.clientX;
+    targetPos.y = e.clientY;
+});
+
+document.querySelectorAll('a, button').forEach(el => {
+    el.addEventListener('mouseover', () => {
+        if (!tooltipEnabled) {
+            document.querySelector('.cursor').classList.add('hover');
+        }
+        document.querySelector('.cursor-inner').classList.add('hover');
+    });
+    el.addEventListener('mouseleave', () => {
+        document.querySelector('.cursor').classList.remove('hover');
+        document.querySelector('.cursor-inner').classList.remove('hover');
+    });
+});
+
+document.addEventListener('wheel', () => {
+    if (!tooltipEnabled) {
+        document.querySelector('.cursor').classList.add('scroll');
+        document.querySelector('.cursor-inner').classList.add('scroll');
+        clearTimeout(scrollTimer);
+        scrollTimer = setTimeout(() => {
+            document.querySelector('.cursor').classList.remove('scroll');
+            document.querySelector('.cursor-inner').classList.remove('scroll');
+        }, 150);
+    }
+});
+
+document.addEventListener('mousedown', () => {
+    document.querySelector('.cursor').classList.add('clicked');
+    document.querySelector('.cursor-inner').classList.add('clicked');
+});
+
+document.addEventListener('mouseup', () => {
+    document.querySelector('.cursor').classList.remove('clicked');
+    document.querySelector('.cursor-inner').classList.remove('clicked');
+});
+
+window.addEventListener('wheel', (e) => {
+    if (e.deltaY > 0) {
+        innerCursorPos.y += 2;
+    } else {
+        innerCursorPos.y -= 2;
+    }
+});
+
+async function updateInnerCursor() {
+    const cursorInner = document.querySelector('.cursor-inner');
+    const dx = (targetPos.x - innerCursorPos.x) * 0.2;
+    const dy = (targetPos.y - innerCursorPos.y) * 0.2;
+
+    innerCursorPos.x += dx;
+    innerCursorPos.y += dy;
+
+    cursorInner.style.left = `${innerCursorPos.x}px`;
+    cursorInner.style.top = `${innerCursorPos.y}px`;
+
+    requestAnimationFrame(updateInnerCursor);
+}
+
+requestAnimationFrame(updateInnerCursor);
+
+function showTooltip(text) {
+    const cursor = document.querySelector('.cursor');
+    cursor.classList.add('tooltip');
+    cursor.innerHTML = text;
+    tooltipEnabled = true;   
+}
+
+function hideTooltip() {
+    const cursor = document.querySelector('.cursor');
+    cursor.classList.remove('tooltip');
+    cursor.innerHTML = '';
+    tooltipEnabled = false;
+    innerCursorPos = {
+        x: targetPos.x,
+        y: targetPos.y
+    };
+
+    cursor.style.left = `${targetPos.x}px`;
+    cursor.style.top = `${targetPos.y}px`;
+}
